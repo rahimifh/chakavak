f9608de10fc2f3b28413cc8ce721aa3172e753b7
unknown <rahimi.f.h@gmail.com>
2021-05-27 09:05:44 +0430

    remove some file

--- chaakavak bach up/chakavak/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body')
-    search_fields = ('title', 'body')
--- chaakavak bach up/chakavak/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name='blog'
-
-urlpatterns = [
-    path('', views.news, name='news'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.news_detail,name='news_detail'),
-
-]
